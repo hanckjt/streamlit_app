a1ebbdc1a3e4c01fa7432271389a8cd62804fe40
Hank <hanckjt@gmail.com>
2023-01-10 02:09:02 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,7 @@
 import streamlit as st
+import streamlit_authenticator as stauth
+
+source_pwd = st.text_input('Source password', key='source_pwd', type='password')
+if st.button('Hash', key='hash_password'):
+    hashed_password = stauth.Hasher([source_pwd]).generate()[0]
+    st.text_input('Hashed password', value=hashed_password, key='hashed_password')
