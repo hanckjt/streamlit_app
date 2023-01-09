06093b631b1b5a3afd267a9e419f49d7f4a71fed
Hank <hanckjt@gmail.com>
2023-01-10 02:35:57 +0800

    Add test code

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,6 @@ source_pwd = st.text_input('Source password', key='source_pwd', type='password')
 if st.button('Hash', key='hash_password'):
     hashed_password = stauth.Hasher([source_pwd]).generate()[0]
     st.text_input('Hashed password', value=hashed_password, key='hashed_password')
+
+st.text_area('AppSecrets', value=st.secrets)
+st.write(st.session_state)
